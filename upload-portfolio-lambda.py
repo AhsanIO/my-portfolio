0c86c756feec6a7d5a1ffb1abb87caabfe95fe90
Ahsan Ahmad <ahsan@finisoft.co.uk>
2018-04-10 12:37:29 +0100

    python script to upload portfolio

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,22 @@
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('portfolio.ahsanahmad.io')
+build_bucket = s3.Bucket('portfoliobuild.ahsanahmad.io')
+
+
+build_bucket.download_file('portfoliobuild.zip', 'c:/Users/Ahsan/Desktop/portfoliobuild.zip')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
