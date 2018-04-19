4c5d29df8dbf0b270092a7c9806296f9bf8a7980
Ahsan Ahmad <ahsan@finisoft.co.uk>
2018-04-19 17:54:46 +0100

    Use SNS to notify when portfolio is deployed

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,25 +4,33 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:828130611904:deployPortfolioTopic')
 
 
-    s3 = boto3.resource('s3')
+    try:
+        s3 = boto3.resource('s3')
 
-    portfolio_bucket = s3.Bucket('portfolio.ahsanahmad.io')
-    build_bucket = s3.Bucket('portfoliobuild.ahsanahmad.io')
+        portfolio_bucket = s3.Bucket('portfolio.ahsanahmad.io')
+        build_bucket = s3.Bucket('portfoliobuild.ahsanahmad.io')
 
 
-    build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
+        build_bucket.download_file('portfoliobuild.zip', '/tmp/portfoliobuild.zip')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
+        print "job done."
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully.")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="Portfolio was NOT deployed successfully.")
+        raise
 
     return 'Hello from Lambda'
